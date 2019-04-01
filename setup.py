cc22be04c7e297c47735ce7802e31a8f4edd394e
gpalou4 <44896814+gpalou4@users.noreply.github.com>
2019-04-02 00:52:13 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -7,5 +7,8 @@ setup(name='Macrocomplex Builder',
 	description='This program is able to reconstruct biological macrocomplexes of protein-protein interactions as well as protein-DNA/RNA interactions given a set of binary interactions and the desired number of chains of the target complex.',
 	author='Guillermo Palou Marquez and Javier Sanchez Utges',
 	author_email='guillepalou4@gmail.com',
+	long_description=open('README.md').read(),
+	install_requires=['biopython >= 1.73.0','argparse >= 1.1.0'],
+	license='LICENSE.txt',
 	url='https://github.com/gpalou4/macrocomplex_builder',
 	scripts=['macrocomplex_builder.py','macrocomplex_functions.py'])
